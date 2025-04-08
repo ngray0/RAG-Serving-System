6dadd786b7873ede689d1828fc233ea93ffe3fa2
Matthew Collins <s2686742@ed.ac.uk>
2025-04-08 11:38:10 +0100

    Test

--- task-1/run_task.sh
+++ task-1/run_task.sh
@@ -1,2 +1,27 @@
-#!/bin/bash
-python task.py
+echo "Job running on ${SLURM_JOB_NODELIST}"
+
+dt=$(date '+%d/%m/%Y %H:%M:%S')
+echo "Job started: $dt"
+
+
+# ===================
+# Environment setup
+# ===================
+
+echo "Setting up bash enviroment"
+
+# Make available all commands on $PATH as on headnode
+source ~/.bashrc
+
+# Make script bail out after first error
+set -e
+
+
+
+CONDA_ENV_NAME=MLSystems
+echo "Activating conda environment: ${CONDA_ENV_NAME}"
+conda activate ${CONDA_ENV_NAME}
+COMMAND="python task_test.py"
+echo "Running provided command: ${COMMAND}"
+eval "${COMMAND}"
+echo "Command ran successfully!"
