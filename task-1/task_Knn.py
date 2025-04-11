5f7d8d923b8e0aec3f10b10ba67b3a473df07046
Matthew Collins <s2686742@ed.ac.uk>
2025-04-11 22:39:38 +0100

    Triton-ANN

--- task-1/task_Knn.py
+++ task-1/task_Knn.py
@@ -772,10 +772,10 @@ def our_knn_stream(N, D, A, X, K):
 # ============================================================================
 if __name__ == "__main__":
     # --- Fixed Parameters ---
-    N_data = 100000
-    N_queries = 1000
+    N_data = 10000
+    N_queries = 100
     K_val = 10          # K for KNN
-    NUM_RUNS = 10       # Number of timed runs for averaging
+    NUM_RUNS = 5       # Number of timed runs for averaging
     WARMUP_RUNS = 2     # Number of warm-up runs
 
     # --- Dimensions to Test ---
