d30639f8ebb544888961b4a75e69556f44ef1f28
Matthew Collins <s2686742@ed.ac.uk>
2025-04-12 16:10:10 +0100

    Testing

--- task-1/task_Knn.py
+++ task-1/task_Knn.py
@@ -851,7 +851,7 @@ def our_knn_stream(N, D, A, X, K):
 # ============================================================================
 if __name__ == "__main__":
     # --- Fixed Parameters ---
-    N_data =4000
+    N_data =4000000
     N_queries = 1
     K_val = 10          # K for KNN
     NUM_RUNS = 10       # Number of timed runs for averaging
