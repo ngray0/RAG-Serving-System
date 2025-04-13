4aad3aa375a3c2310aa27de4f17d3319352bdcd3
Matthew Collins <s2686742@ed.ac.uk>
2025-04-13 10:47:03 +0100

    Test

--- task-1/run_task.sh
+++ task-1/run_task.sh
@@ -35,7 +35,7 @@ rsync --archive --update --compress --progress ${data_path}/MLS-G31 ${SCRATCH_HO
 
 
 
-COMMAND="python -u task_Ann_Cupy2.py"
+COMMAND="python -u task_Ann_CUpy2.py"
 echo "Running provided command: ${COMMAND}"
 eval "${COMMAND}"
 echo "Command ran successfully!"
